e5c21b75995fbc2e90aeb29927ab39bcb75b3ad0
Jason Huey <jason.huey1@gmail.com>
2018-07-11 20:40:25 -0700

    update

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,18 @@
-console.log("connected");
+function todoList() {
+	var username = document.getElementById("username").value
+	var comment = document.getElementById("comment").value
+
+	var userNameText = document.createTextNode("Name: " + username)
+	var commentText = document.createTextNode("Comment: " + comment)
+
+	var newListItem = document.createElement("LI")
+	newListItem.className = "list-group-item"
+	var newUserName = document.createElement("H5")
+	newUserName.appendChild(userNameText)
+	var newComment = document.createElement("P")
+	newComment.appendChild(commentText)
+
+	newListItem.appendChild(newUserName)
+	newListItem.appendChild(newComment)
+	document.getElementById("commentList").appendChild(newListItem)
+}
